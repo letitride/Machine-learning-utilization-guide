9138191b3e26f8da669ce503eae50f69f073d680
letitride <ichikawa.fumiya@gmail.com>
2020-01-25 10:36:54 +0900

    5-4 ファイルアップロードの実装

--- 5/WebApp/app.py
+++ 5/WebApp/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = './static/images/upload'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.secret_key = 'flash_key'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルを選択して下さい')
+            return render_template('index.html')
+
+        file = request.files['file']
+        if not allowed_file(file.filename):
+            flash('拡張子はpng, jpg, jpegのみ使用可能です')
+            return render_template('index.html')
+        else:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(file_path)
+            return render_template('display_img.html', file_path=file_path)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
